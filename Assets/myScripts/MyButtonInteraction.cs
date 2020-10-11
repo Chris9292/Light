041c747de710b1ed033c846380f6733682518b4d
Driexus <72216504+Driexus@users.noreply.github.com>
2020-10-11 16:48:44 +0300

    Cylinder/Colliders/ActiveBool

---
+++ Assets/myScripts/MyButtonInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class MyButtonInteraction : PressableButtonHoloLens2
+{
+    public UnityEvent OnMyButtonClicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ButtonPressed.AddListener(MyMethod);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MyMethod()
+    {
+        Debug.Log("aa");
+    }
+}
