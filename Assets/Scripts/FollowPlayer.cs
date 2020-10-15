569f288566e5039ccdf9aab5bc2ee2cdbdc1c26c
Christos Kagkelidis <xrh555@gmail.com>
2020-10-15 18:49:57 +0300

    added menu

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject camera;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        offset = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = camera.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    
+    public int health;
+    public int pulse;
+    public int threshold;
+
+    public int getHealth()
+    {
+        return this.health;
+    }
+
+    
+
+}
