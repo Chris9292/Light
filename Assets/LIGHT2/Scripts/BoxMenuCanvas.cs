b8d5e6c0531a4617041c046ad077f992a8093415
Driexus <72216504+Driexus@users.noreply.github.com>
2020-12-17 19:37:28 +0200

    Initial Commit
    ~ Main Menu Manager
    Activates a GameObject (MainMenu) on double click
    
    ~ Box Menu
    Simple Quad Menu that follows you
    
    ~ No Cursor Profile
    
    ~ Fixed Editor Bug where the editor would crash if it lost input source on remote connecting
    ! Dont put interactable objects under Main Camera hyrierachy
    
    ~ Created LIGHT2 Folder

---
+++ Assets/LIGHT2/Scripts/BoxMenuCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMenuCanvas : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        //transform.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LIGHT2/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private bool clickedOnce = false;
+    public float doubleClickTime = 1f;
+    public GameObject mainMenu = null;
+
+    private void Awake()
+    {
+        if (mainMenu == null)
+        {
+            throw new UnityException("GameObject MainMenu cannot be null");
+        }    
+    }
+
+    public void OpenMainMenu()
+    {
+        if (clickedOnce == true)
+        {
+            mainMenu.SetActive(true);
+        }
+        else
+        {
+            StartCoroutine(ClickedOnce());
+        }
+    }
+
+    IEnumerator ClickedOnce()
+    {
+        clickedOnce = true;
+        yield return new WaitForSeconds(doubleClickTime);
+        clickedOnce = false;
+    }
+}
