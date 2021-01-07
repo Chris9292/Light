6d0899013a99c9bc9e046c4bfea17f5f2e4039bf
ThanAsk <72217014+ThanAsk@users.noreply.github.com>
2021-01-07 16:00:22 +0200

    Local Connection test

---
+++ Assets/ReceiveSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveSource : MonoBehaviour
+{
+    [SerializeField] private Microsoft.MixedReality.WebRTC.Unity.VideoRenderer videoRenderer;
+    [SerializeField] private Microsoft.MixedReality.WebRTC.Unity.VideoReceiver videoReceiver;
+    public void StartRender()
+    {
+        videoRenderer.StartRendering(videoReceiver.VideoTrack);
+    }
+
+    // Update is called once per frame
+    public void StopRender()
+    {
+        videoRenderer.StopRendering(videoReceiver.VideoTrack);
+    }
+}
---
+++ Assets/RenderSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderSource : MonoBehaviour
+{
+    [SerializeField] private Microsoft.MixedReality.WebRTC.Unity.VideoRenderer videoRenderer;
+    [SerializeField] private Microsoft.MixedReality.WebRTC.Unity.WebcamSource webcamSource;
+    
+    public void StartVideoStream()
+    {
+        videoRenderer.StartRendering(webcamSource.Source);
+    }
+
+    
+    public void StopVideoStream()
+    {
+        videoRenderer.StopRendering(webcamSource.Source);
+    }
+}
