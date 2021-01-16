661493f48dd4b2add857597a918726a6091dd80d
Christos Kagkelidis <xrh555@gmail.com>
2021-01-16 17:09:45 +0200

    Main menu / gaze (#10)
    * Cylinder/Colliders/ActiveBool
    
    * Near/Far
    
    * button works on release
    
    * button works on release
    
    * HoverEntered/Exited + 4 Cubes
    
    * radial view
    
    * Cleanup
    
    * Photo Capture/ Selection Router/ Working Build
    
    * Cleanup
    
    Removed Vudoria and other non-used packages and cleared up scripts and comments
    
    * Erased not needed solutions
    
    Renamed main solution to PhotoCaptureApp
    
    * Initial Commit
    
    ~ Main Menu Manager
    Activates a GameObject (MainMenu) on double click
    
    ~ Box Menu
    Simple Quad Menu that follows you
    
    ~ No Cursor Profile
    
    ~ Fixed Editor Bug where the editor would crash if it lost input source on remote connecting
    ! Dont put interactable objects under Main Camera hyrierachy
    
    ~ Created LIGHT2 Folder
    
    * Cleanup + Comments
    
    * Spatial Mapping Settings
    
    * First Hologram Menu
    
    * Enable/Disable Menu
    
    HologramDisplay script added
    
    * Revert "Merge branch 'MainMenu-/-Gaze' of https://github.com/Chris9292/Light into MainMenu-/-Gaze"
    
    This reverts commit 3771c038cb0113ca4731d0489f76120ebc63e8ce, reversing
    changes made to 98f102f0b866916e3792d169f009e1e2539bd71d.
    
    * Added capsule prefab
    
    * update Sample scene
    
    * TapToPlace Try
    
    * Revert "Merge branch 'main' into MainMenu-/-Gaze"
    
    This reverts commit ac56818f90ffce34b42005d3d1c2292a13eb9bc6, reversing
    changes made to 0a4c552a33369c7bac08cc8168815d2a6b581ef3.
    
    * Revert "Merge branch 'main' into MainMenu-/-Gaze"
    
    This reverts commit f00f51c695e3d119c0c8868b7822f77cd79dfb5b, reversing
    changes made to ac56818f90ffce34b42005d3d1c2292a13eb9bc6.
    
    Co-authored-by: Driexus <72216504+Driexus@users.noreply.github.com>
    Co-authored-by: NikitasME <45981821+NikitasME@users.noreply.github.com>

---
+++ Assets/HologramDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramDisplay : MonoBehaviour
+{
+        GameObject Display;
+    //Transform DisplayTransform = Display.transform;
+    public int CurrentHolo = 0;
+
+//change to onEnable
+    private void OnEnable(){
+        //Display = GameObject.FindGameObjectWithTag("HologramPlate);
+        Display = transform.GetChild(0).gameObject;
+
+         //First Hologram in Hierarchy Enabled ; All the rest disabled
+        for (int i=0; i<=Display.transform.childCount-1; i++){
+        Display.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        Display.transform.GetChild(CurrentHolo).gameObject.SetActive(true);
+    }
+
+    
+
+    public void DisplayNext(){
+        Display.transform.GetChild(CurrentHolo).gameObject.SetActive(false);
+
+        CurrentHolo += 1;
+        if (CurrentHolo >= Display.transform.childCount)
+        {
+        CurrentHolo = 0;
+            }
+         Display.transform.GetChild(CurrentHolo).gameObject.SetActive(true);
+    
+    }
+
+
+    
+
+    public void DisplayPrevious(){
+        Display.transform.GetChild(CurrentHolo).gameObject.SetActive(false);
+
+        CurrentHolo -= 1;
+        if (CurrentHolo < 0)
+        {
+        CurrentHolo = Display.transform.childCount - 1;
+            }
+         Display.transform.GetChild(CurrentHolo).gameObject.SetActive(true);
+    }
+
+
+}
