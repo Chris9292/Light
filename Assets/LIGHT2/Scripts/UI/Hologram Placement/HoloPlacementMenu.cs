db14347d86b6f84cccf6dc366a3e543fcefa0a44
Driexus <72216504+Driexus@users.noreply.github.com>
2021-02-27 13:06:33 +0200

    SamplingHolo and HoloPlacement scripts

---
+++ Assets/LIGHT2/Scripts/UI/Hologram Placement/HoloPlacementMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloPlacementMenu : MonoBehaviour
+{
+    public Texture2D photo;
+    public string holoName;
+    public string description;
+    public string color;
+    public string numberOfRocks;
+
+    GameObject samplingHolo;
+    public GameObject SamplingHolo;
+    public enum HologramType { sampling, sitescreening };
+    public HologramType hologramType = HologramType.sampling;
+
+    private void Awake()
+    {
+        samplingHolo = Resources.Load("Sampling Hologram") as GameObject;
+        SamplingHolo = Instantiate(samplingHolo);
+    }
+
+    public void Place()
+    {
+        if (hologramType == HologramType.sampling)
+        {
+            //SamplingHolo
+        }
+    }
+
+    public void NextHolo()
+    {
+        if (hologramType == HologramType.sampling)
+            hologramType = HologramType.sitescreening;
+        else
+            hologramType = HologramType.sampling;
+    }
+}
