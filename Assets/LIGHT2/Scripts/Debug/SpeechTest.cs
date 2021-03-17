0c10e3436e7c0a59b068daa336424e1e5611e868
Driexus <72216504+Driexus@users.noreply.github.com>
2021-03-17 19:16:46 +0200

    SpeechInput Debug

---
+++ Assets/LIGHT2/Scripts/Debug/SpeechTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SpeechTest : MonoBehaviour
+{
+    public TMP_Text resultText;
+
+    public void SpeechInputDetected()
+    {
+        resultText.text = "Speech Input Detected!";
+    }
+
+    public void ResetTest()
+    {
+        resultText.text = "Waiting for speech input...";
+    }
+}
