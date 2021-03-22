4ebdd095bba936b6f91aa683ff09f5c7c220a99b
unknown <xrh555@gmail.com>
2021-03-23 00:49:13 +0200

    object pool added

---
+++ Assets/LIGHT2/Scripts/Navigation/ObjectPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool SharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(objectToPool);
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public void DeactivatePoolObjects()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            if (pooledObjects[i].activeInHierarchy)
+            {
+                pooledObjects[i].SetActive(false);
+            }
+        }
+    }
+    
+}
+
+
+
