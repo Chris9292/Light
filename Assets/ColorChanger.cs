7759506ff8aa380277678276e4814279289e36ab
george-21 <gpdc0021@gmail.com>
2021-03-22 18:19:37 +0200

    Stoneo task

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public MeshRenderer rend;
+    public Material[] mats;
+    public int cur;
+
+    private void Start()
+    {
+        if (rend == null)
+        {
+            rend = GetComponent<MeshRenderer>();
+        }
+    }
+
+    public void RandomColor()
+    {
+        rend.material.color = UnityEngine.Random.ColorHSV();
+    }
+}
