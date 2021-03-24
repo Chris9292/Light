d09c26917988e8c921ea120229a6347a7f3187fb
ThanAsk <72217014+ThanAsk@users.noreply.github.com>
2021-03-25 00:42:51 +0200

    PressAnimation Script started

---
+++ Assets/LIGHT2/Scripts/UI/PressAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressedMove : MonoBehaviour
+{
+    [SerializeField] private Transform button;
+    [SerializeField] private float MoveDuration;
+    private float elapsed;
+    private float startValue;
+    private float endValue;
+    float valueToLerp;
+    
+
+
+    private void Awake()
+    {
+        startValue = button.localScale.x;
+        endValue = button.localScale.x - 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Move());
+    }
+
+    void Move()
+    {
+        while(elapsed < MoveDuration)
+        {
+            valueToLerp = Mathf.Lerp(startValue,endValue,elapsed/MoveDuration);
+                yield return null;
+        }
+
+        valueToLerp = endValue;
+    }
+}
