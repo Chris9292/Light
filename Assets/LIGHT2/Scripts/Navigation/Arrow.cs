5335a74148c3ed776350bdc2bb8d4081cc28a5f7
Chris9292 <xrh555@gmail.com>
2021-04-03 15:05:17 +0300

    temp

--- Assets/LIGHT2/Scripts/Navigation/Arrow.cs
+++ Assets/LIGHT2/Scripts/Navigation/Arrow.cs
@@ -6,10 +6,12 @@ public class Arrow : MonoBehaviour
 {
     GameObject dynamicPathObject;
 
+    // arrow object that points to the nearest node on the path
+    // tbd
     // Start is called before the first frame update
     void Start()
     {
-        
+        // test arrow
     }
 
     // Update is called once per frame
