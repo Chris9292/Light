cbdf373e6abc80397dd8901073a0c7dbbb34b418
Driexus <72216504+Driexus@users.noreply.github.com>
2021-04-27 18:59:54 +0300

    TooltipCreator update

---
+++ Assets/LIGHT2/Scripts/UI/MiniMap/MinimapIcon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIcon : MonoBehaviour
+{
+    public Texture2D mainTex;
+    Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.SetTexture("_MainTex", mainTex);
+    }
+}
--- Assets/LIGHT2/Scripts/UI/TooltipCreator.cs
+++ Assets/LIGHT2/Scripts/UI/TooltipCreator.cs
@@ -2,14 +2,25 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class TooltipCreator : MonoBehaviour
 {
     GameObject resourcesTooltip;
     GameObject tooltip;
 
+    [Tooltip("Check this in order for the tooltip to remain until object loses focus")]
+    public bool remainIndefinite;
+    [Tooltip("Only relevant if remainIndefinite is set to false")]
+    public float activeTime = 5f;
+
+    public int fontSize = 36;
+
     [Tooltip("Tooltip's local position")]
     public Vector3 tooltipPosition = new Vector3();
+
+    public string tooltipText;
 
     private void Awake()
     {
@@ -22,6 +33,11 @@ public class TooltipCreator : MonoBehaviour
         tooltip.transform.SetParent(transform);
         tooltip.transform.localPosition = tooltipPosition;
         tooltip.transform.rotation = transform.rotation;
+
+        TMP_Text tmp = tooltip.GetComponent<TMP_Text>();
+        tmp.text = tooltipText;
+        tmp.fontSize = fontSize;
+
     }
 
     // Check if we have a Button or Interactable component and AddListeners
@@ -41,6 +57,15 @@ public class TooltipCreator : MonoBehaviour
             entry.eventID = EventTriggerType.PointerExit;
             entry.callback.AddListener((data) => tooltip.SetActive(false));
             trigger.triggers.Add(entry);
+
+            // Turn off after activeTime if remainIndefinite is false
+            if (!remainIndefinite)
+            {
+                entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.PointerEnter;
+                entry.callback.AddListener((data) => StartCoroutine(DisableTooltipAfterSeconds(activeTime)));
+                trigger.triggers.Add(entry);
+            }
         }
         else if (TryGetComponent(out Interactable interactable))
         {
@@ -50,10 +75,20 @@ public class TooltipCreator : MonoBehaviour
 
             focusReceiver.OnFocusOn.AddListener(() => tooltip.SetActive(true));
             focusReceiver.OnFocusOff.AddListener(() => tooltip.SetActive(false));
+
+            // Turn off after activeTime if remainIndefinite is false
+            if (!remainIndefinite)
+                focusReceiver.OnFocusOn.AddListener(() => StartCoroutine(DisableTooltipAfterSeconds(activeTime)));
         }
         else
         {
             throw new UnityException("No Button or Interactable component found in " + gameObject);
         }
+    }
+
+    IEnumerator DisableTooltipAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        tooltip.SetActive(false);
     }
 }
