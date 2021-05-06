58a8738b92ed5c8b1c367eca07d57ef251a606dd
Driexus <72216504+Driexus@users.noreply.github.com>
2021-05-06 15:55:34 +0300

    Spatial Testing

--- Assets/LIGHT2/Scripts/myTest.cs
+++ Assets/LIGHT2/Scripts/myTest.cs
@@ -1,11 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Microsoft.MixedReality.Toolkit;
+using UnityEngine.XR.WSA.Persistence;
 
 public class myTest : MonoBehaviour
 {
-    public void PrintTest()
+  /*  WorldAnchorStore store;
+    string savedRoot;
+
+    private void Awake()
     {
-        Debug.Log("Focused");
+        WorldAnchorStore.GetAsync(StoreLoaded);
+        savedRoot = Application.dataPath;
     }
+    private void SaveGame()
+    {
+        
+        // Save data about holograms positioned by this world anchor
+        if (!this.savedRoot) // Only Save the root once
+        {
+            this.savedRoot = this.store.Save("rootGameObject", anchor);
+            Assert(this.savedRoot);
+        }
+    }
+    private void LoadGame()
+    {
+        // Save data about holograms positioned by this world anchor
+        this.savedRoot = this.store.Load("rootGameObject", rootGameObject);
+        if (!this.savedRoot)
+        {
+            // We didn't actually have the game root saved! We have to re-place our objects or start over
+        }
+    }
+    private void StoreLoaded(WorldAnchorStore store)
+    {
+        this.store = store;
+    }*/
 }
---
+++ Assets/Scenes/Spatial/ColorTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour
+{
+    public Color color;
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
+}
