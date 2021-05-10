872bfaa69c03e52f1863b2e478dac3cb73fe337f
Chris9292 <xrh555@gmail.com>
2021-05-10 06:41:55 +0300

    Networking HTTP service added / ready for testing

---
+++ Assets/LIGHT2/Scripts/Networking/Model/API_Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+public class API_Response
+{
+    public int id { get; set; }
+    public int s_id { get; set; }
+    public string name { get; set; }
+    public double value { get; set; }
+    public DateTime date { get; set; }
+}
+
---
+++ Assets/LIGHT2/Scripts/Networking/WebRequest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine.Networking;
+ 
+public class WebRequest: MonoBehaviour
+{
+
+    
+    public string url;
+    public TextMeshProUGUI responseTextMeshPro;
+
+    void Start()
+    {
+        responseTextMeshPro.text = $"Sending Http Request to following URL: {url}\n\n";
+    }
+    public void NetworkAPI()
+    {
+        StartCoroutine(GetServerData());
+    }
+ 
+    private IEnumerator GetServerData()
+    {
+        responseTextMeshPro.text = $"Sending Http Request to following URL: {url}\n\n";
+        
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+
+        if (www.isHttpError || www.isNetworkError) {
+            Debug.Log("Error: " + www.error);
+            responseTextMeshPro.text += $"Error: {www.error}";
+        }
+        else {
+            
+            var jsonResponse = JsonConvert.DeserializeObject<API_Response>(www.downloadHandler.text);
+            
+            responseTextMeshPro.text += $"ID : {jsonResponse.id}" +"\n";
+            responseTextMeshPro.text += $"S_ID : {jsonResponse.s_id}" + "\n";
+            responseTextMeshPro.text += $"Name : {jsonResponse.name}" +"\n";
+            responseTextMeshPro.text += $"Value : {jsonResponse.value}" +"\n";
+            responseTextMeshPro.text += $"Date : {jsonResponse.date}" +"\n\n";
+            responseTextMeshPro.text += $"HTTP status code: {www.responseCode}";
+        }
+    }
+    
+}
