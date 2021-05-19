93b3d05a81ea56eb1c90d0cedca2d3b94eb5bc9b
Driexus <72216504+Driexus@users.noreply.github.com>
2021-05-19 16:15:15 +0300

    Metrics Calculations

---
+++ Assets/LIGHT2/Scripts/UI/Metrics/Bar.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Bar : MonoBehaviour
+{
+    private double val = 0;
+    private double maxVal = 0;    
+    
+    //Reference to slider component of healthbar (fill element)
+    [SerializeField] private Image metricImage = null;
+
+    [SerializeField] private Metrics metrics = null;
+
+    public TMP_Text valueDisplay;
+    public string metricUnits;
+
+
+    private float LerpUpdateSpeed = 0.2f;
+
+    private double percent = 1f;
+
+    private void Start()
+    {
+
+        //maxVal = metrics.maxHeartRate;
+        //Debug.Log(maxVal);
+        string Name = gameObject.name;
+        Debug.Log(Name);
+        switch (Name)
+        {
+            case "Heart Rate":
+                maxVal = metrics.maxHeartRate;
+                break;
+            case "Oxygen Supply":
+                maxVal = metrics.maxOxygenTime;
+                break;
+            case "Metabolic Rate":
+                maxVal = metrics.maxMetabolicRate;
+                break;
+            case "Suit Pressure":
+                maxVal = metrics.maxSuitPressure;
+                break;
+            default:
+                maxVal = 100;
+                break;
+        }
+        val = maxVal;
+
+    }
+
+    private void Update()
+    {
+        GetVal();
+        StartCoroutine(ChangeVal(percent));
+        valueDisplay.text = val.ToString() + " " + metricUnits;
+    }
+
+    private void GetVal()
+    {
+        switch (gameObject.name)
+        {
+            case "Heart Rate":
+                val = metrics.HeartRate;
+                break;
+            case "Oxygen Supply":
+                val = metrics.OxygenTime;
+                break;
+            case "Metabolic Rate":
+                val = metrics.MetabolicRate;
+                break;
+            case "Suit Pressure":
+                val = metrics.SuitPressure;
+                break;
+            default:
+                val = 50.50;
+                break;
+        }
+
+        percent = val / maxVal;
+    }
+
+    private IEnumerator ChangeVal(double percent)
+    {
+        float elapsed = 0f;
+        float currPercent = metricImage.fillAmount;
+
+        while (elapsed < LerpUpdateSpeed)
+        {
+            elapsed += Time.deltaTime;
+            metricImage.fillAmount = Mathf.Lerp(currPercent, (float)percent, elapsed / LerpUpdateSpeed);
+
+            yield return null;
+        }
+        metricImage.fillAmount = (float)percent;
+    }
+}
---
+++ Assets/LIGHT2/Scripts/UI/Metrics/Metrics.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+
+public class Metrics : MonoBehaviour
+{
+    //Grid GridReference;
+    private double velocity;  // m/s
+
+    ///Biometrics
+    
+    // Constants
+    public double avgHeartRate = 90;
+    public float depletionRate = 1;
+    [SerializeField] private float slope = 0;      // degrees
+    [SerializeField] private double mass = 0;       // Kg
+    [SerializeField] private double g = 0;    // Kg/s
+
+    // Properties
+    public double MetabolicRate {get; private set; } //  Joule/m
+    public double maxMetabolicRate { get; private set; }
+
+    public double HeartRate{get; private set; }  //  bpm 
+    public double maxHeartRate { get; private set; }
+    public bool IsCalm { get; private set; }
+
+    public float OxygenTime { get; private set; } // minutes to oxygen depletion
+    public float maxOxygenTime { get; private set; }
+    public bool timerIsRunning { get; private set; } //Timer control
+
+    public double SuitPressure { get; private set; }
+    public double maxSuitPressure { get; private set; }
+
+
+    // Update Counter
+    [Tooltip("Interval between metric updates")]
+    public float updateTime;
+
+    private float counter;
+
+    private void Awake()
+    {
+        //GridReference = GetComponent<Grid>;  
+        //slope = 1; //degrees 
+        timerIsRunning = true;
+        IsCalm = true;
+        maxHeartRate = 160;
+
+    }
+
+    void Start()
+    {
+        counter = 1f;
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= updateTime)
+        {
+            CalculateHeartRate();
+            CalculateMetabolicRate();
+            CalculateOxygen();
+            CalculateSuitPress();
+            CalculateVelocity();
+
+            counter = 0f;
+        }
+    }
+
+    //Approximate astronaut velocity and metabolic rate relations (Santee et al.2001)
+
+    private void CalculateVelocity()
+    {
+        if (slope < -20 || slope > 20)
+            velocity = 0;
+        else if (slope <= -10)
+            velocity = 0.095 * slope + 1.95;
+        else if (slope < 0)
+            velocity = 0.05 * slope + 1.6;
+        else if (slope < 6)
+            velocity = -0.2 * slope + 1.6;
+        else if (slope < 15)
+            velocity = 0.039 * slope + 0.634;
+        else
+            velocity = 0.05;
+    }
+
+    private void CalculateMetabolicRate()
+    {
+        float slope_sin = Mathf.Sin(slope);
+        float slope_cos = Mathf.Cos(slope);
+        float slope_abs = Mathf.Abs(slope);
+
+        double LevelEnergy = (3.28 * mass + 71.1) * (0.661 * velocity * slope_cos + 0.115);
+        double SlopeEnergy;
+        if (slope > 0)
+            SlopeEnergy = 3.5 * mass * g * velocity * slope_sin;
+        else if (slope < 0)
+            SlopeEnergy = 2.4 * mass * g * velocity * slope_sin * Mathf.Pow(0.3f, (float)(slope_abs / 7.65));
+        else
+        {
+            SlopeEnergy = 0;
+        }
+
+
+        MetabolicRate = LevelEnergy + SlopeEnergy;
+    }
+
+    private void CalculateOxygen()
+    {
+        if (timerIsRunning)
+        {
+            if (OxygenTime > 0)
+            {
+                OxygenTime -= depletionRate * Time.deltaTime;
+            }
+            else
+            {
+                OxygenTime = 0;
+                timerIsRunning = false;
+
+            }
+        }
+    }
+
+    public void CalculateHeartRate()       //avg: 60-100 , range: 60-160
+    {
+        if (IsCalm)
+        {
+            //HeartRate = avgHeartRate + Mathf.Cos(Random.Range(-5,5));
+            HeartRate = avgHeartRate + Random.Range(-5, 5);
+        }
+
+        //HeartRate = avgHeartRate + 50 + Mathf.Cos(Random.Range(-5,5));
+        HeartRate = avgHeartRate + Random.Range(-5, 5);
+    }
+
+    private void CalculateSuitPress()
+    {
+
+    }
+
+    // int GetManhattanDist(Node nodeA,Node nodeB){
+    // int ix = Mathf.Abs(nodeA.iGridX - nodeB.iGridX);//x1-x2
+    // int iy = Mathf.Abs(nodeA.iGridY - nodeB.iGridY);//y1-y2
+
+    // return ix + iy;//Return the sum
+    // }
+
+}
